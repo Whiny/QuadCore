de5c645cfdf55f6f43532024e683e4a8f4428dc0
jandy14_ <cbs3080@daum.net>
2016-08-03 19:43:24 +0900

    공격 스크립트

---
+++ QuadCore/Assets/Script/Player/test/ControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerTest : MonoBehaviour
+{
+	private float delta = 0;
+    public float PlayerMovementSpeed = 10;
+    public float PlayerRotationSpeed = 90;
+    void Update()
+    {
+        UserInputs();
+    }
+    void UserInputs()
+    {
+        Debug.Log(Input.GetButtonDown("P1_A Button"));
+		Debug.Log(Input.GetAxis("P1_LeftThumbstickButton"));
+		Debug.Log(Input.GetButton("P1_A Button"));
+	}
+}
