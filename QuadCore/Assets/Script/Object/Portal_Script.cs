c8885fc48d982f248e72c7fa3b6ce1f52c360292
hookssi <sounghoo12@gmail.com>
2016-08-03 21:59:04 +0900

    Portal, moving ground added

---
+++ QuadCore/Assets/Script/Object/Portal_Script.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Portal_Script : Object
+{
+    public Transform red; // 빨간색 문
+    public Transform green; // 초록색 문
+
+    private BoxCollider2D redColider; // 빨간색 문 콜라이더
+    private BoxCollider2D greenColider; // 초록색 문 콜라이더
+    private bool isExit;
+
+	void Start()
+    {
+        isExit = false;
+        redColider = gameObject.AddComponent<BoxCollider2D>();
+        redColider.size = new Vector2(0.277f, 0.35f);
+        redColider.offset = red.position;
+        redColider.isTrigger = true;
+
+        greenColider = gameObject.AddComponent<BoxCollider2D>();
+        greenColider.size = new Vector2(0.277f, 0.35f);
+        greenColider.offset = green.position;
+        greenColider.isTrigger = true;
+    }
+
+    protected override void Active(Collider2D other)
+    {
+        if (!isExit)
+        {
+            if (redColider.IsTouching(other))
+                other.transform.position = green.position;
+            if (greenColider.IsTouching(other))
+                other.transform.position = red.position;
+
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, -other.GetComponent<Rigidbody2D>().velocity.y);
+            isExit = true;
+        }
+        else if (isExit)
+            isExit = false;
+    }
+}
--- QuadCore/Assets/Script/Player/Attack.cs
+++ QuadCore/Assets/Script/Player/Attack.cs
@@ -59,9 +59,9 @@ public class Attack : MonoBehaviour
 	{
 		if (other.tag == "player")
 		{
-			if (/*플레이어가 공격상태이면*/)
+			if (/*플레이어가 공격상태이면*/true)
 			{
-				other.GetComponent<PlayerControl>().Damaged(currentSwingIsPower);
+				//other.GetComponent<PlayerControl>().Damaged(currentSwingIsPower);
 			}
 		}
 	}
