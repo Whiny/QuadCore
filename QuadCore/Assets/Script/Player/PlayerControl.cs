2671fa7fe65d40d681af6ebe6efc875614de6d31
zxc99as <zxc99as@naver.com>
2016-08-03 13:29:32 +0900

    플레이어 스크립트 생성

---
+++ QuadCore/Assets/Script/Player/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+	private int direction;
+	private float time;
+	private float speed;
+	private float jump;
+	private bool isStun;
+	private bool isPlaying;
+	private bool isJumpping;
+
+	// Use this for initialization
+	void Start ()
+	{
+		time = 0;
+		speed = 200;
+		jump = 400;
+
+		isStun = false;
+		isJumpping = false;
+		isPlaying = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Move();
+	}
+
+	void Move()
+	{
+
+	}
+}
