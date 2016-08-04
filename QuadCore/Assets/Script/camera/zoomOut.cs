7c510c7840e3fe48a428e1608e1ff21a65761933
nan-anya <rjsxkr1122@naver.com>
2016-08-04 17:13:47 +0900

    카메라 단 4개!

---
+++ QuadCore/Assets/Script/camera/zoomOut.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoomOut : MonoBehaviour
+{
+    Camera camera1;
+
+    Camera camera2;
+
+    Camera camera3;
+
+    Camera camera4;
+
+    void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+	
+	}
+}
