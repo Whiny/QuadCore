24af6019230510e95a93fcdd25fc5b9ef67d4df6
zxc99as <zxc99as@naver.com>
2016-08-09 23:11:41 +0900

    * 플레이어 1 피해 애니메이션 추가 * 플레이어 2 애니메이션 제작 완료 * 차지 공격 완벽히 수정

--- QuadCore/Assets/Script/Player/PlayerAttack.cs
+++ QuadCore/Assets/Script/Player/PlayerAttack.cs
@@ -1,36 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerAttack : PlayerControl
+public class PlayerAttack : MonoBehaviour
 {
 	public GameObject root_Object;
 
+	private float delay;
+	private bool isEnable;
+
 	void Start ()
 	{
-	
+		delay = 0;
+		this.GetComponent<PolygonCollider2D>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-	
+		if(delay >= 0.1f)
+		{
+			delay = 0;
+			isEnable = false;
+
+			this.GetComponent<PolygonCollider2D>().enabled = false;
+			root_Object.GetComponent<PlayerControl>().Power = 4;
+		}
+		if (isEnable) delay += Time.deltaTime;
 	}
 
 	private void OnTriggerEnter2D(Collider2D col)
 	{
-		
 		if (col.gameObject.tag == "Player")
 		{
 			if (col.gameObject.name == "CollisionCollider")
 			{
-				Debug.Log("ASDF");
 				int angle = root_Object.GetComponent<PlayerControl>().Angle;
 				float power = root_Object.GetComponent<PlayerControl>().Power;
-				root_Object.GetComponent<PlayerControl>().Power = 4f;
+				Debug.Log(power);
 
 				if (angle == 0) col.gameObject.GetComponentInParent<PlayerControl>().Damaged(power, 1);
 				else col.gameObject.GetComponentInParent<PlayerControl>().Damaged(power, -1);
 			}
 		}
+	}
+
+	public void DelayOn()
+	{
+		delay = 0;
+		isEnable = true;
 	}
 }
