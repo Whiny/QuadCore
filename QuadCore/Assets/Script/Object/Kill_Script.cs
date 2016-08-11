4bbc7c91b9ca3709b27d950778f478de7982238c
BackSeockJun <tkdldjstm785@gmail.com>
2016-08-11 17:05:57 +0900

    맵수정 (MovingObject + RotationfGround 추가 , 1층 변경)

---
+++ QuadCore/Assets/Script/Object/Kill_Script.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kill_Script : Object {
+
+    protected override void Active( Collider2D other )
+    {
+        other.transform.gameObject.GetComponentInParent<NewPlayerControl>().Die();
+    }
+}
