79eaba55c335a0e2dccf877f17da3c1de10c1c2e
jandy14_ <cbs3080@daum.net>
2016-08-12 17:52:28 +0900

    메인메뉴 씬 제작

--- QuadCore/Assets/Script/GameManagement/GameManager.cs
+++ QuadCore/Assets/Script/GameManagement/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System;
 
@@ -19,7 +20,10 @@ public class GameManager : MonoBehaviour
 		int winner = -1;
 		//혹시나 에러 날까봐(사실은 그냥 써보고 싶어서)
 		try { index = Convert.ToInt16(p_Name[1]); }
-		finally{ Debug.Log("Index Error"); index = 0; }
+		finally{ Debug.Log("Index Error"); index = -1; }
+
+		//에러면 게임 종료
+		if (index == -1) { GameOver(index); return; }
 
 		isPlaying[index] = false;
 
@@ -39,10 +43,19 @@ public class GameManager : MonoBehaviour
     }
 	private void GameOver(int winner)
 	{
-		if (winner == -1) { Debug.Log("Winner Number Error"); }
+		if (winner > 3 || winner < 0)
+		{
+			Debug.Log("Winner Number Error");
+			GameObject.Find("/Canvas/Text").GetComponent<Text>().text = "It's Error";
+		}
 		else
 		{
-			 //게임 승리시
+			//게임에서 승리한 플레이어의 카메라를 전체로 확대하고 승리문구 출력
+			//GameObject.Find(/*오브젝트 이름*/).GetComponentInChildren<Camera>().rect = new Rect (0, 0, 1, 1);
+			GameObject.Find("/Canvas/Text").GetComponent<Text>().text = winner + " Palyer\nWin!";
 		}
+
+		//게임 종료
+		Application.LoadLevel("MainMenu");
 	}
 }
